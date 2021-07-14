35bbbaf9123f886221693d0e9e916a03bc14e96f
Luciel Santos <luciel.vitor.bs@gmail.com>
2021-07-14 16:22:19 -0300

    add shimmer on pokemon image

--- src/components/PokemonImg/PokemonImg.tsx
+++ src/components/PokemonImg/PokemonImg.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { getPokemonImageByUrl, getPokemonIdByUrl } from '../../utils/generic';
+import { shimmerPlaceholder } from '../../utils/shimmerPlaceholder';
 
 interface IProps {
   url: string;
@@ -7,6 +8,13 @@ interface IProps {
 
 export const PokemonImg = ({ url }: IProps) => {
   return (
-    <Image src={getPokemonImageByUrl(url)} height={80} width={80} alt={getPokemonIdByUrl(url)} />
+    <Image
+      src={getPokemonImageByUrl(url)}
+      height={80}
+      width={80}
+      alt={getPokemonIdByUrl(url)}
+      placeholder="blur"
+      blurDataURL={shimmerPlaceholder(80, 80)}
+    />
   );
 };
